f8aa96f600f4ab3f6df6fb171dc42ee00a1d8796
Bam Fisher <fisherbam@gmail.com>
2020-10-01 19:48:19 -0500

    My first commit

---
+++ app/src/main/java/com/example/gpsapp/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.gpsapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends Activity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+//Find your views
+       button2 = (Button) findViewById(R.id.button);
+
+        //Assign a listener to your button
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1){
+                //Start your second activity
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+}}
